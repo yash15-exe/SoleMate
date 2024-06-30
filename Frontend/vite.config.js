a26d37e77fe5e3001be91e3a21d1e36eca5ae38a
yash15-exe <yashraikar153@gmail.com>
2024-06-30 12:40:14 +0530

    Added cors

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server:{
     proxy:{
-      "/api":"https://solemate-api.vercel.app"
+      "/api/*":"https://solemate-api.vercel.app"
     }
   }
 })
